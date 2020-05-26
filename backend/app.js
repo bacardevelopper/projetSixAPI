120f02bcce0561e047378afe681d8825a0758a07
bacardevelopper <bacar.darwin.github@hotmail.com>
2020-05-26 21:25:59 +0200

    enr° correct° router

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,24 @@
-const express = require('express');
+const express = require("express");
+const mongoose = mongoose();
+const bodyParser = require("body-parser");
+
+/* connection à mongodb */
+mongoose
+  .connect("mongodb://localhost/my_database", {
+    useNewUrlParser: true,
+    useUnifieldTopology: true,
+  })
+  .then(() => {
+    console.log("connection reussit à mongodb");
+  })
+  .catch(() => {
+    console.log("connection echoué à mongodb");
+  });
+
+
 const app = express();
-
 app.use((req, res) => {
-    res.status(200).json({msg : 'serveur connecté'});
+  res.status(200).json({ msg: "serveur connecté" });
 });
-
 
 module.exports = app;
--- backend/router.js
+++ backend/router.js
@@ -1,3 +1,2 @@
-const router = require('express');
+const express = require("express");
 const router = express.Router();
-
