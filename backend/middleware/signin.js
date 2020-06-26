c49aa99a9a33d740bf6fa215229de686d80703ce
bacardevelopper <bacar.darwin.github@hotmail.com>
2020-06-26 13:56:54 +0200

    ajout des tokens d'authentification

--- backend/middleware/signin.js
+++ backend/middleware/signin.js
@@ -1,38 +1,47 @@
-const bcrypt = require('bcrypt');
-const UserModel = require('../userModel');
+const bcrypt = require("bcrypt");
+const UserModel = require("../userModel");
+const jwt = require('jsonwebtoken');
 /*                                      */
 
 exports.loginUser = (req, res, next) => {
+  if (req.body.email !== "") {
 
-	if(req.body.email !== ""){
+    UserModel.findOne({ email: req.body.email })
+      .then((user) => {
+        if (user) {
+          /* on compare les mot de passe si c'est me bon */
+          bcrypt
+            .compare(req.body.mdp, user.mdp)
+            .then((valid) => {
+              if (valid) {
+                /* retourner un token */
+                return res.status(200)
+                  .json({
 
-		UserModel.findOne({email : req.body.email})
-			.then((user) => {
-				if(user){
-					
-					/* on compare les mot de passe si c'est me bon */
-					bcrypt.compare(req.body.mdp, user.mdp)
-						.then( (valid) => {
-							if(valid){
-								return res.status(200).json({message : 'uutilisateur trouvé et bon mdp'});
-							}
-					})
-						.catch( (error) => {
-								return res.status(401);
-					});
+                  	message: "utilisateur trouvé et bon mdp",
+                   	userId : user._id, 
+                  	token : jwt.sign({userId : user._id}, 'TOKEN_IS_FREE_OPEN_SOURCE',
+                  	{expiresIn : '24h'}) 
+                  	
 
-				}else{
-					return res.status(401).json({error : 'utilisateur non trouvé'});
-				}
+                  });
+              }
+            })
+            .catch((error) => {
+              return res.status(401);
+            });
+        } else {
+          return res.status(401).json({ error: "utilisateur non trouvé" });
+        }
+      })
+      .catch((error) => {
+        res
+          .status(500)
+          .json({ error: "erreur verifier que vos id correspondent" });
+      });
 
-		})
-		.catch((error) => {
-			res.status(500).json({error : 'erreur verifier que vos id correspondent'});
-		});
-
-	}else{
-		res.status(500).json({ message : 'champ vide'});
-		console.log('champ vide');
-	}
-	
-}
+  } else {
+    res.status(500).json({ message: "champ vide" });
+    console.log("champ vide");
+  }
+};
--- backend/middleware/signup.js
+++ backend/middleware/signup.js
@@ -1,5 +1,5 @@
-const bcrypt = require('bcrypt');
-const UserModel = require('../userModel');
+const bcrypt = require("bcrypt");
+const UserModel = require("../userModel");
 /*                                   */
 
 const saltRounds = 10;
@@ -8,44 +8,47 @@ const saltRounds = 10;
 /* ## step 1: verify value !== "" , step 2: regex email, step 3: hash password, 
 step 4: save in mongoodb */
 exports.createUser = (req, res, next) => {
-	console.log('reussit middleware');
-	if(req.body.email !== "" && req.body.mdp !== ""){
-		if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(req.body.email)){
-			bcrypt.hash(req.body.mdp, saltRounds)
-				.then(hash => {
-					console.log('Le hash  :'+hash +' et son :'+req.body.email);
-					
-					req.body.mdp = hash;
-					const userValue = req.body
+  console.log("reussit middleware");
+  if (req.body.email !== "" && req.body.mdp !== "") {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(req.body.email)) {
+      bcrypt
+        .hash(req.body.mdp, saltRounds)
+        .then((hash) => {
+          console.log("Le hash  :" + hash + " et son :" + req.body.email);
 
-					const user = new UserModel({
-						email : req.body.email ,
-						mdp : hash
-					});
-					/* ## step 4: save in mongodb */
-					user.save()
-						.then(()=>{
-							return res.status(201).send();
-							console.log('user create');	
-						})
-						.catch(()=>{
-							return res.status(400).send();
-							console.log('erreur');
-						});
-						
-					console.log('user modeliser '+ user + 'la suite en bas'+ '\n');
+          req.body.mdp = hash;
+          const userValue = req.body;
 
-					UserModel.find({},  (err, docs) => {
-						console.log(docs); // affich un à un les données du bdd
-					});
-				})
-				.catch(error => {
-					console.log(error);	
-				});
-		}
-	}else{
-		console.log("error sur les champs");	
-	}
+          const user = new UserModel({
+            email: req.body.email,
+            mdp: hash,
+          });
 
-	res.end();
-}
+          /* ## step 4: save in mongodb */
+          user
+            .save()
+            .then(() => {
+              return res.status(201).send();
+              console.log("user create");
+            })
+            .catch(() => {
+              return res.status(400).send();
+              console.log("erreur");
+            });
+
+          console.log("user modeliser " + user + "la suite en bas" + "\n");
+
+          UserModel.find({}, (err, docs) => {
+            console.log(docs); // affich un à un les données du bdd
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  } else {
+    console.log("error sur les champs");
+  }
+
+  res.end();
+};
