0591e4df179f7e95f0164ecb88ac8b157b16aff3
bacardevelopper <bacar.darwin.github@hotmail.com>
2020-07-06 12:39:30 +0200

    delete img

--- backend/controllers/returnallsauces.js
+++ backend/controllers/returnallsauces.js
@@ -3,7 +3,8 @@ const modelSauce = require('../model/sauceModel');
 exports.returnAll = (req, res, next) => {
 	modelSauce.find({}, (err, docs) => {
 		if(!err){
-			res.status(200).json({docs});
+			res.status(200).json(docs);
+
 			console.log(docs);	
 		}
 
